126b703b1ec3a9cac2a48cdeebe266533caacce5
Gabriel <extofer@gmail.com>
2012-10-27 06:24:52 -0600

    added Events EF, controller and views

---
+++ app/BearCamp/BearCamp/Controllers/EventsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BearEF;
+
+namespace BearCamp.Controllers
+{ 
+    public class EventsController : Controller
+    {
+        private bearcampEntities db = new bearcampEntities();
+
+        //
+        // GET: /Events/
+
+        public ViewResult Index()
+        {
+            return View(db.Events1.ToList());
+        }
+
+        //
+        // GET: /Events/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Events events = db.Events1.Find(id);
+            return View(events);
+        }
+
+        //
+        // GET: /Events/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Events/Create
+
+        [HttpPost]
+        public ActionResult Create(Events events)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events1.Add(events);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(events);
+        }
+        
+        //
+        // GET: /Events/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Events events = db.Events1.Find(id);
+            return View(events);
+        }
+
+        //
+        // POST: /Events/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Events events)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(events).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(events);
+        }
+
+        //
+        // GET: /Events/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Events events = db.Events1.Find(id);
+            return View(events);
+        }
+
+        //
+        // POST: /Events/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Events events = db.Events1.Find(id);
+            db.Events1.Remove(events);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}

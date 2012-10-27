73a572e9716c0df6cd81be72fb0d3bd2aeaa0bf4
Kelly Martinez <anthem001@gmail.com>
2012-10-27 00:44:59 -0600

    added favicon

--- app/BearCamp/BearCamp/Global.asax.cs
+++ app/BearCamp/BearCamp/Global.asax.cs
@@ -20,6 +20,7 @@ namespace BearCamp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("favicon.ico");
 
             routes.MapRoute(
                 "Default", // Route name
